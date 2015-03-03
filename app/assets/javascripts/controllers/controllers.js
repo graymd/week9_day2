7e78d359da3a2396808e5a6eb28c6fea750a07bf
Malachi Gray <graymd.cfi@gmail.com>
2015-03-03 16:06:18 -0500

    able to create, read, and update objects with angular

--- app/assets/javascripts/controllers/controllers.js
+++ app/assets/javascripts/controllers/controllers.js
@@ -20,38 +20,16 @@ var indexCtrl = addressBook.controller('indexCtrl', function($scope, contactData
       $scope.formPhoneNumber = ''
     };
 
-    $scope.deleteContact = function(quoteId) {
-      contactData.deleteContact(quoteId);
-    };
+    $scope.deleteContact = function(contactId) {
+      contactData.deleteContact(contactId);
+    }
+
+    $scope.editContact = function(contact) {
+      
+      $scope.formFirstName = contact.first_name
+      $scope.formLastName = contact.last_name
+      $scope.formEmail = contact.email
+      $scope.formPhoneNumber = contact.phone_number
+      // contactData.deleteEditedContact(contact);
+    }
 });
-
-
-
-
-
-// var indexCtrl = addressBook.controller('indexCtrl', function($scope, quoteData) {
-//     $scope.some_text = {quote: "'eeeeeeyyyyyy!", quoteMaster: "The Fonz"};
-//     $scope.quotes = quoteData.data;
-//     $scope.formQuote = '';
-//     $scope.formQuoteMaster = '';
-//     quoteData.loadQuotes();
-//     $scope.changeQuote = function() {      
-//       $scope.some_text = _.sample($scope.quotes.quotes);
-//     };
-
-//     $scope.submitForm = function() {
-//       quoteData.addQuote(
-//         {
-//           quote: {
-//             quote: $scope.formQuote, quote_master: $scope.formQuoteMaster 
-//           }
-//         }
-//       );
-//       $scope.formQuote = '';
-//       $scope.formQuoteMaster = '';
-//     };
-
-//     $scope.deleteQuote = function(quoteId) {
-//       quoteData.deleteQuote(quoteId);
-//     }
-// });
--- app/assets/javascripts/factories/factories.js
+++ app/assets/javascripts/factories/factories.js
@@ -15,9 +15,32 @@ addressBook.factory('contactData', function($http) {
     });
   }
 
+  contactData.addContact = function(contact) {
+    $http.post("/contacts.json", contact).success(function(contactFromServer){
+      contactData.pushContact(contactFromServer);
+    })
+  }
+
   contactData.pushContact = function(contact) {
     contactData.data.contacts.push(contact);
   }
+
+  contactData.deleteContact = function(contactId) {
+    $http.delete("/contacts/" + contactId + ".json").success(function(data){
+      console.log("Success");
+      var deletedContact = _.findWhere( contactData.data.contacts, {id: parseInt(contactId)})
+      console.log(contactData.data.contacts)
+      contactData.data.contacts = _.without(contactData.data.contacts, deletedContact)
+      console.log(contactData.data.contacts)
+    })
+  }
+
+  // contactData.editContact = function(contact) {
+  //   console.log(contact.id)
+  //   $http.patch("/contacts" + contact.id + ".json").success(function(data){
+      
+  //   })
+  // }
 
 return contactData;
 });
